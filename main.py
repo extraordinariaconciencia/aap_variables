da0e2fca1be4d40592f04a845771f54545f7c488
reklu <sergio.ser.ema@gmail.com>
2019-12-24 00:35:42 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+saludo = "Hola "
+nombre = input("Ingresa nombre la concha de tu hermana: ")
+
+print(saludo + nombre)
+'''
+'''
+num1 = input("Ingrese el primer numero: ")
+num2 = input("Ingrese segundo numero: ")
+
+print(num1 + num2)
+'''
+
+nombre = input("ingrese nombre:")
+print(nombre)
